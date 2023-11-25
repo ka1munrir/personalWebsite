74a11f9e98f73b6eb9391add0792c045bfc00bd9
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-11-24 19:14:27 -0700

    added skill

--- src/app/(generalPages)/about/page.jsx
+++ src/app/(generalPages)/about/page.jsx
@@ -25,6 +25,7 @@ export default function About() {
     ['React.js', reactIcon],
     ['Vite.js', viteIcon],
     ['Next.js', nextjsIcon],
+    ['React Native', reactIcon],
     ['SQL', sqlIcon],
     ['SQLAlchemy', sqlalchemyIcon],
     ['Flask', flaskIcon],
