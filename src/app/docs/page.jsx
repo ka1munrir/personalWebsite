82d0693cb578bb86b908a945313bf8b3800e6e6a
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-11-26 21:38:27 -0700

    worked a little on docs home

--- src/app/docs/page.jsx
+++ src/app/docs/page.jsx
@@ -3,6 +3,9 @@ import styles from './page.module.css'
 
 export default function Docs() {
   return (
-    <h1 className={styles.title}>Docs</h1>
+    <>
+      <h1 className={`${styles.title}`}>The Archives</h1>
+      <p className={`${styles.text}`}>These archives are a compilation of my current knowledge of programming</p>
+    </>
   )
 }
