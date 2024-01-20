a23d05b43cb56e9a13cde5872792a9a8909468da
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2024-01-19 18:02:57 -0700

    change home page content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <div className={`${styles.textContainer} ${grid.homeTextContainer}`}>
         <h1 className={styles.title}> Hey, I'm <br /> Juan <br />Ortiz-Acosta</h1>
-        <p className={styles.shortDesc}>I'm a software engineer intern and a computer<br /> science student at the University of New Mexico.</p>
+        <p className={styles.shortDesc}>I'm a software engineering intern and a computer<br /> science student at the University of New Mexico.</p>
       </div>
       <div className={`${styles.imageContainer} ${grid.homeProfilePicContainer}`}>
         <Image
