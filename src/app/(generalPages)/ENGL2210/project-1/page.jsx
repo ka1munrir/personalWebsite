872ec26c21cca19e0f52b216aa39ca845f2d3410
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2024-02-10 00:35:01 -0700

    add more to bio

--- src/app/(generalPages)/ENGL2210/project-1/page.jsx
+++ src/app/(generalPages)/ENGL2210/project-1/page.jsx
@@ -10,11 +10,15 @@ export default function Project_1() {
         <h1 className={`${styles.text}`}>Bio</h1>
         <p className={`${styles.text}`}>Juan Ortiz-Acosta is a computer science student at the University of New Mexico 
         with extensive full stack software engineering training from Flatiron School. While he initially 
-        decided to study electrical engineering, his interest in circuits turned into a passion for programming. 
-        He has experience developing mobile and web applications using Python, JavaScript, Java, SQL, JSON, HTML, 
-        CSS, and many web frameworks. He loves problem solving, collaborating, and learning new things. His 
-        interests lie in technologies ranging from web, mobile, and app development to machine learning and 
-        artificial intelligence.</p>
+        decided to study electrical engineering at Case Western Reserve University, his interest in circuits 
+        turned into a passion for programming. He gained experience in Python, JavaScript, Java, SQL, JSON, 
+        HTML, CSS, and many web frameworks in Flatiron School where he built many web applications and a 
+        mobile application, and he learned Java during his time at Case Western Reserve University. He loves 
+        problem solving, collaborating, and learning new things of which he is currently learning Japanese, C, 
+        C++, drawing, philosophy, and piano. Juan&#39;s current interests lie in technologies ranging from 
+        web, mobile, and computer app development to machine learning and artificial intelligence. He is 
+        currently working on a Python desktop application to expand his knowledge of Python and delve into 
+        his interest of computer app development.</p>
         
     </>
   )
